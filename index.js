99e6d6bb648317caf4a0e5a8cb730983659f9b8c
ubermensh <srinathshrestha9890@gmail.com>
2024-11-15 13:15:25 +0530

    Removed Slashes in fileName

--- index.js
+++ index.js
@@ -23,89 +23,90 @@ functions.cloudEvent('transcodeOnUpload', async (cloudEvent) => {
     const inputUri = `gs://${file.bucket}/${file.name}`;
     const outputUri = `gs://movie-streaming-hls-output/${file.name.split('.')[0]}/`;
 
-    const job = {
-      inputUri: inputUri,
-      outputUri: outputUri,
-      config: {
-        elementaryStreams: [
-          {
-            key: 'video_360p',
-            videoStream: {
-              h264: {
-                heightPixels: 360,
-                widthPixels: 640,
-                bitrateBps: 400000,
-                frameRate: 30,
-              },
+      const job = {
+    inputUri: inputUri,
+    outputUri: `${outputUri}`, // Base output URI, such as 'gs://movie-streaming-hls-output/cap-vs-bucky/',
+    config: {
+      elementaryStreams: [
+        {
+          key: 'video_360p',
+          videoStream: {
+            h264: {
+              heightPixels: 360,
+              widthPixels: 640,
+              bitrateBps: 400000,
+              frameRate: 30,
             },
           },
-          {
-            key: 'video_720p',
-            videoStream: {
-              h264: {
-                heightPixels: 720,
-                widthPixels: 1280,
-                bitrateBps: 2500000,
-                frameRate: 30,
-              },
+        },
+        {
+          key: 'video_720p',
+          videoStream: {
+            h264: {
+              heightPixels: 720,
+              widthPixels: 1280,
+              bitrateBps: 2500000,
+              frameRate: 30,
             },
           },
-          {
-            key: 'video_1080p',
-            videoStream: {
-              h264: {
-                heightPixels: 1080,
-                widthPixels: 1920,
-                bitrateBps: 5000000,
-                frameRate: 30,
-              },
+        },
+        {
+          key: 'video_1080p',
+          videoStream: {
+            h264: {
+              heightPixels: 1080,
+              widthPixels: 1920,
+              bitrateBps: 5000000,
+              frameRate: 30,
             },
           },
-        ],
-        muxStreams: [
-          {
-            container: 'ts',
-            elementaryStreams: ['video_360p'],
-            fileName: '360p/segments/segment_%04d.ts',
-            key: 'hls_360p',
-            segmentSettings: {
-              segmentDuration: {
-                seconds: 6, // Corrected to an object
-              },
+        },
+      ],
+      muxStreams: [
+        {
+          container: 'ts',
+          elementaryStreams: ['video_360p'],
+          fileName: '360p_segment_%04d.ts', // No slashes
+          key: 'hls_360p',
+          segmentSettings: {
+            segmentDuration: {
+              seconds: 6,
             },
           },
-          {
-            container: 'ts',
-            elementaryStreams: ['video_720p'],
-            fileName: '720p/segments/segment_%04d.ts',
-            key: 'hls_720p',
-            segmentSettings: {
-              segmentDuration: {
-                seconds: 6, // Corrected to an object
-              },
+        },
+        {
+          container: 'ts',
+          elementaryStreams: ['video_720p'],
+          fileName: '720p_segment_%04d.ts', // No slashes
+          key: 'hls_720p',
+          segmentSettings: {
+            segmentDuration: {
+              seconds: 6,
             },
           },
-          {
-            container: 'ts',
-            elementaryStreams: ['video_1080p'],
-            fileName: '1080p/segments/segment_%04d.ts',
-            key: 'hls_1080p',
-            segmentSettings: {
-              segmentDuration: {
-                seconds: 6, // Corrected to an object
-              },
+        },
+        {
+          container: 'ts',
+          elementaryStreams: ['video_1080p'],
+          fileName: '1080p_segment_%04d.ts', // No slashes
+          key: 'hls_1080p',
+          segmentSettings: {
+            segmentDuration: {
+              seconds: 6,
             },
           },
-        ],
-        manifests: [
-          {
-            fileName: 'master.m3u8',
-            type: 'HLS',
-            muxStreams: ['hls_360p', 'hls_720p', 'hls_1080p'],
-          },
-        ],
-      },
-    };
+        },
+      ],
+      manifests: [
+        {
+          fileName: 'master.m3u8',
+          type: 'HLS',
+          muxStreams: ['hls_360p', 'hls_720p', 'hls_1080p'],
+        },
+      ],
+    },
+  };
+
     
     const [response] = await transcoderClient.createJob({ parent, job });
     console.log(`Job ${response.name} created for file ${file.name}`);
